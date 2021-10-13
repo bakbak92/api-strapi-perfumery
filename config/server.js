48661b995fd82a57706a9f7bdea3f7a63ccc2da9
bakbak92 <bakarycoly92@gmail.com>
2021-10-14 00:26:11 +0200

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3f6eff917b92cbd114359d692dfc4ac5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '285197e6-ef12-4125-9af9-243b06fbaefd'
+};
